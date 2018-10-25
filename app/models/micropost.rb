18a4ae8fb3627b834fd9813ff95b09f7f2dcc1c9
r_kyo <hige3157@gmail.com>
2018-10-25 16:13:42 +0000

    2.3.3 practice completed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :conente, length: { maximum: 140 }
+  validates :conente, length: { maximum: 140 },
+                      presence: true
 end
